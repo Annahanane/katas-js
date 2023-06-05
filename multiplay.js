fba3b123a83990c3efdbbc58c29a4e2631b057c8
AnnaHanane <rawan1710@hotmail.com>
2023-06-05 15:26:18 +0200

    katas -string-

--- multiplay.js
+++ multiplay.js
@@ -1,8 +1,21 @@
-function evenOrOdd(number) {
-    if (number % 2 == 0){
-      return "Even"
-    } else{
-      return "Odd"
-    };
-    
-  }
+ function evenOrOdd(number) {
+     if (number % 2 == 0){
+       return "Even"
+     } else{
+       return "Odd"
+      };
+  }  
+ 
+ /////////Reversed Strings///////
+ function solution(str){
+    let arryStr = str.split("");
+     let arry = arryStr.reverse();
+     let str1 = arry.join("");
+     return str1;
+     
+     
+   }
+ /////////// Remove First and Last Character//////// 
+   function removeChar(str){
+    return str.substring(1, str.length-1);
+   }; 
